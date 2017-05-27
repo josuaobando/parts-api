916bef1a4788be50f7fe69997a748f81aca738d5
josuaobando <Josh2909>
2017-05-26 23:42:04 -0600

    add flag

--- cronjobs/start.php
+++ cronjobs/start.php
@@ -2,9 +2,17 @@
 
 require_once('system/Startup.class.php');
 
-// Start the Cron job
-$connector = new Connector();
-$response = $connector->loadContent(CoreConfig::CRON_JOB_SERVICES);
+if(CoreConfig::CRON_JOBS_ACTIVE)
+{
+  // Start the Cron job
+  $connector = new Connector();
+  $response = $connector->loadContent(CoreConfig::CRON_JOB_SERVICES);
 
-var_dump($response);
+  var_dump($response);
+}
+else
+{
+  echo 'Services are turned off!';
+}
+
 ?>
